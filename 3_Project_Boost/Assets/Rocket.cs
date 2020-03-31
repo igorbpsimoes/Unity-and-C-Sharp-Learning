7a72176ae91da06d993242023de526c964fd76c9
Igor Simões <ibps@cin.ufpe.br>
2020-03-31 12:48:56 -0300

    Movement added & .gitignore file

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if(Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward);
+        } else if(Input.GetKey(KeyCode.D)) {
+            transform.Rotate(Vector3.back);
+        }
     }
 }
