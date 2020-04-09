fa0dddd8746c0bd2dd5d046e9daf1c14e9a7a4dc
Igor Simões <ibps@cin.ufpe.br>
2020-04-09 11:31:03 -0300

    Add level manager and planet rotation scripts

---
+++ 4_Argon_Assault/Assets/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(this.gameObject.scene.buildIndex == 0) {
+            if (Input.anyKeyDown) {
+                Invoke("LoadFirstLevel", 1f);
+            }
+        }
+    }
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene(1);
+    }
+}
