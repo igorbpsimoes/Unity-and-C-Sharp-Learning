d3a055923e8fb6812ec4664b327db1e5d854191f
Igor Simões <ibps@cin.ufpe.br>
2020-04-10 10:56:33 -0300

    Fix curly brackets position

--- 4_Argon_Assault/Assets/RotationOnPlanet.cs
+++ 4_Argon_Assault/Assets/RotationOnPlanet.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationOnPlanet : MonoBehaviour
-{
+public class RotationOnPlanet : MonoBehaviour {
     // Start is called before the first frame update
     Quaternion startRotation;
-    void Start()
-    {
+    void Start() {
         startRotation = transform.rotation;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
