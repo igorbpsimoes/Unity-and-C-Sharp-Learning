07a7ef6e18ac2bf5ec108b4cbdfb417aa367c496
Igor Simões <ibps@cin.ufpe.br>
2020-04-10 10:59:37 -0300

    Add CrossPlatformInput package and player script

---
+++ 4_Argon_Assault/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In m/s")][SerializeField] float xSpeedFactor = 4f;
+    
+    float xThrow, xOffset;
+    
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xOffset = xSpeedFactor * Time.deltaTime;
+    }
+}
