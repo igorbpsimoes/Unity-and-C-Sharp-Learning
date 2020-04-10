b1035514249b2332ddf6ebb4857ce5208aeef14e
Igor Simões <ibps@cin.ufpe.br>
2020-04-10 13:26:54 -0300

    Refactor functions

--- 4_Argon_Assault/Assets/Player.cs
+++ 4_Argon_Assault/Assets/Player.cs
@@ -1,20 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip("In m/s")][SerializeField] float xSpeedFactor = 15f;
+    //Speed factor
+    [Tooltip("In m/s")][SerializeField] float speed = 15f;
+    //Screen limits
     [Tooltip("In m")][SerializeField] float xMax = 15f;
     [Tooltip("In m")] [SerializeField] float xMin = -15f;
-
-    [Tooltip("In m/s")] [SerializeField] float ySpeedFactor = 15f;
     [Tooltip("In m")] [SerializeField] float yMax = 5f;
     [Tooltip("In m")] [SerializeField] float yMin = -10f;
 
     float xThrow, xOffset, xRawPos, xClampedPos;
     float yThrow, yOffset, yRawPos, yClampedPos;
+
+    //Rotation Order = Y > X > Z
+
     // Start is called before the first frame update
     void Start() {
 
@@ -22,22 +26,28 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        HandleHorizontalMovement();
-        HandleVerticalMovement();
+        ProcessTranslation();
+        ProcessRotation();
     }
 
-    private void HandleHorizontalMovement() {
+    private void ProcessTranslation() {
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        xOffset = xSpeedFactor * xThrow * Time.deltaTime;
-        xRawPos = transform.localPosition.x + xOffset;
-        xClampedPos = Mathf.Clamp(xRawPos, xMin, xMax);
-        transform.localPosition = new Vector3(xClampedPos, transform.localPosition.y, transform.localPosition.z);
-    }
-    private void HandleVerticalMovement() {
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        yOffset = ySpeedFactor * yThrow * Time.deltaTime;
+        
+        xOffset = speed * xThrow * Time.deltaTime;
+        yOffset = speed * yThrow * Time.deltaTime;
+        
+        xRawPos = transform.localPosition.x + xOffset;
         yRawPos = transform.localPosition.y + yOffset;
+        
+        //Limits spaceship position to fit inside screen
+        xClampedPos = Mathf.Clamp(xRawPos, xMin, xMax);
         yClampedPos = Mathf.Clamp(yRawPos, yMin, yMax);
-        transform.localPosition = new Vector3(transform.localPosition.x, yClampedPos, transform.localPosition.z);
+
+        transform.localPosition = new Vector3(xClampedPos, yClampedPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation() {
+        throw new NotImplementedException();
     }
 }
