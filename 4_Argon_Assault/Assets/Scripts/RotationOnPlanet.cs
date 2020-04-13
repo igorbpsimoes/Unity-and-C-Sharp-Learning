eb468be8786dd060f8da50d8f138c1db7135f5a2
Igor Simões <ibps@cin.ufpe.br>
2020-04-13 12:48:28 -0300

    Organize assets folder

--- 4_Argon_Assault/Assets/Scripts/RotationOnPlanet.cs
+++ 4_Argon_Assault/Assets/Scripts/RotationOnPlanet.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotationOnPlanet : MonoBehaviour {
-    // Start is called before the first frame update
-    Quaternion startRotation;
-    void Start() {
-        startRotation = transform.rotation;
-    }
-
+    [SerializeField] float rotationSpeed = 50f;
     // Update is called once per frame
     void Update() {
-        
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
 }
--- 4_Argon_Assault/Assets/Scripts/SceneLoader.cs
+++ 4_Argon_Assault/Assets/Scripts/SceneLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
+public class SceneLoader : MonoBehaviour {
 
     void Awake() {
         DontDestroyOnLoad(gameObject);
