d9d1b0d1b758ec7a87d8c4f299dda6c23b55466a
Igor Simões <ibps@cin.ufpe.br>
2020-04-16 11:52:12 -0300

    Add particle collision system to bullets and separate enemy model from prefab

---
+++ 4_Argon_Assault/Assets/Scripts/ParticleCollisionHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollisionHandler : MonoBehaviour {
+    // Start is called before the first frame update
+    void OnParticleCollision(GameObject other) {
+        transform.parent.gameObject.SendMessage("TakeDamage");
+    }
+}
