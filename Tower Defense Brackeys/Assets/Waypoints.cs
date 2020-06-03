8f14fb5c7b684769028e2b0f6e75330d44e78320
Igor Simões <ibps@cin.ufpe.br>
2020-06-03 19:02:44 -0300

    Make waypoints referenceable by enemies

---
+++ Tower Defense Brackeys/Assets/Waypoints.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Waypoints : MonoBehaviour {
+
+    public static Transform[] points;
+
+    void Awake() {
+        points = gameObject.GetComponentsInChildren<Transform>();
+    }
+}
